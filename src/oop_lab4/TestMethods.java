8356029b0e09c2be4ee0465bc8c28edbdd4aa913
Wimonrat051 <an_za507@hotmail.com>
2017-12-19 09:21:52 +0700

    Test Methods

---
+++ src/oop_lab4/TestMethods.java
@@ -0,0 +1,38 @@
+package oop_lab4;
+
+public class TestMethods {
+    public static void main(String[] args) {
+        //Method
+        // type 1
+        public static void A () {
+            System.out.println("Hello A");
+
+        }//A
+        //type 2
+        public static void B (int x) {
+        System.out.println("Hello B"+x);
+        }
+
+
+        //type 3
+        public static int C (int x,int y){
+            System.out.println("Hello C");
+
+            return x+y;
+        }
+
+
+        public static void main (String[] args){
+
+
+        System.out.println("Hello Main");
+        //calling method
+        //A();
+        //B(10);
+       int number = C(10,20);
+       System.out.println();(number);
+       System.out.println(C(50,50));
+}
+
+    }
+
