8070b33d7e6adf4b8abc1c437f4facb098b042bd
Wimonrat051 <an_za507@hotmail.com>
2017-12-27 16:41:21 +0700

    myArray2D

---
+++ src/oop_lab5/myArray2D.java
@@ -0,0 +1,45 @@
+package oop_lab5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class myArray2D {
+    public static void main(String[] args) throws IOException {
+        //array 2D
+        int[][] num = new  int[2][3];
+        num[0][0] = 100;
+        num[0][2] = 200;
+        num = inputData(num); //ผู้ใช่ใส่ข้อมูลเก็บที่numเรียกใช้ตรงไหนรีเทินตรงนั้น
+        showData (num);
+
+    }//main
+
+    private static int[][] inputData(int[][] num) throws IOException {
+        BufferedReader reader=new BufferedReader( new InputStreamReader(System.in));
+        System.out.println("Enter an integer: ");
+        for (int i=0;i<num.length;i++) {//วนแถวแนวนอน
+            for (int j = 0; j < num[i].length; j++) { //วนแถวแนวตั้ง
+                System.out.print("num[" + i + "][" + j + "]: " );
+                num[i][j]= Integer.parseInt(reader.readLine());
+
+
+
+            }//j
+        }//i
+
+        return num;
+    }//inputData
+
+    private static void showData(int[][] num) {
+        System.out.println("Data in array: ");
+        for (int i=0;i<num.length;i++){//วนแถวแนวนอน
+                for (int j=0;j<num[i].length;j++){ //วนแถวแนวตั้ง
+                    System.out.print(num[i][j]+" ");
+
+
+                }//j
+            System.out.println();
+        }//i
+    }//showData
+}//class
